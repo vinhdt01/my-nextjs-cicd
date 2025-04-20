637197b883221527025189f539f83efd0ed3cdb2
vinhdt06 <vinh.dt06@pnj.com.vn>
2025-04-20 10:19:49 +0700

    up

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/site",
+  images: {
+    minimumCacheTTL: 31536000, // 1 year
+  },
 };
 
 export default nextConfig;
